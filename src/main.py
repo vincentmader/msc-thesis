14ca5bc643c08c073ff5b1756c54e8ae535d4187
Vincent C. Mader <vincent.mader@icloud.com>
2022-09-30 17:05:12 +0200

    initialized state: all particles have mass=1, i.e. n(1)=1, n(x)=0 for x!=1

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+import numpy as np
+
+from config import X_MIN, X_MAX, GRID_RESOLUTION
+
+
+def initialize_state():
+    x = np.linspace(X_MIN, X_MAX, GRID_RESOLUTION)
+    n = np.array([1] + [0] * (GRID_RESOLUTION-1))
+    return x, n
+
 
 def main():
-    print("Hello, World!")
+    x, n0 = initialize_state()
 
 
 if __name__ == "__main__":

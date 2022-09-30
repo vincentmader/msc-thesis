884e6d3577faf87a1db4177f4ce1f8216768e424
Vincent C. Mader <vincent.mader@icloud.com>
2022-09-30 17:15:28 +0200

    implemented plotting of initial state (bar-plot)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import matplotlib.pyplot as plt
 import numpy as np
 
 from config import X_MIN, X_MAX, GRID_RESOLUTION
@@ -10,8 +11,18 @@ def initialize_state():
     return x, n
 
 
+def plot_state(x, n):
+    plt.bar(x, n)
+    plt.title("particle mass distribution")
+    plt.xlabel("mass $x$")
+    plt.ylabel("abundancy $n(x)$")
+    plt.xlim(X_MIN-1, X_MAX+1)
+    plt.show()
+
+
 def main():
     x, n0 = initialize_state()
+    plot_state(x, n0)
 
 
 if __name__ == "__main__":

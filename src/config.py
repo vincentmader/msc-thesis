f2b00570964693381d304e0c18c1ff8eb76f38a9
Vincent C. Mader <vincent.mader@icloud.com>
2022-10-07 13:57:08 +0200

    minor changes to config

--- src/config.py
+++ src/config.py
@@ -3,14 +3,15 @@ NR_OF_TIMESTEPS = 4
 DT              = 1
 
 # Define discretization in mass.
-X_MIN, X_MAX    = 0, 10
-GRID_RESOLUTION = 10
+X_MIN, X_MAX    = 0, 100
+GRID_RESOLUTION = 100
 
 # Define initial state. 
 # Options:
 #  - "gaussian"
 #  - "dirac-delta"
-INITIAL_STATE   = "dirac-delta"  
+# INITIAL_STATE   = "dirac-delta"  
+INITIAL_STATE   = "gaussian"  
 
 # Define paths to figure & data directories.
 PATH_TO_FIGURES = "./figures"

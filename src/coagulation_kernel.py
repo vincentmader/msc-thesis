5ca8a0e7aa4d68679afb6b7a2582cbe159134de4
Vincent C. Mader <vincent.mader@icloud.com>
2022-10-10 11:11:54 +0200

    defined total coagulation kernel

--- src/coagulation_kernel.py
+++ src/coagulation_kernel.py
@@ -1,6 +1,9 @@
 from numba import jit
 import utils
 
+def K(k, i, j):
+    return K_gain(k, i, j) - K_loss(k, i, j)
+
 @jit(nopython=True)
 def K_gain(k, i, j):
     return 1/2 * utils.kronecker_delta(k, i+j)

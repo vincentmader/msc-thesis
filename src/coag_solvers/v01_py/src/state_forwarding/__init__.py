c36aaa2ba587ca68f1794a608632ed42ebcdfa21
Vincent C. Mader <vincent.mader@icloud.com>
2022-11-09 16:50:29 +0100

    minor

--- src/coag_solvers/v01_py/src/state_forwarding/__init__.py
+++ src/coag_solvers/v01_py/src/state_forwarding/__init__.py
@@ -4,25 +4,27 @@ from numba import jit
 
 @jit(nopython=True, cache=True)
 def dn_k(K_gain, K_loss, n, k):
-    dn_k = 0
+    out = 0
 
-    for i in range(len(n)):
-        for j in range(len(n)):
+    for i, _ in enumerate(n):
+        for j, _ in enumerate(n):
             gain = 1/2 * K_gain[k][i][j] * n[i] * n[j]
-            dn_k += gain
+            out += gain
 
     for j, _ in enumerate(n):
         i = k
         loss = K_loss[k][i][j] * n[i] * n[j]
-        dn_k += loss
+        out += loss
 
-    return dn_k
+    return out
 
 
 @jit(nopython=True, cache=True)
 def forward_state(K_gain, K_loss, x, n):
+
     # Initialize mass-distribution derivative vector.
     dn = np.zeros(len(x))
+
     # Calulcate entries of derivative vector.
     for k, _ in enumerate(n):
         dn[k] = dn_k(K_gain, K_loss, n, k)

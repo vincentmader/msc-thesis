e7a9891e43431f724ed0eb37df181b2a2cda6c04
Vincent C. Mader <vincent.mader@icloud.com>
2022-11-14 17:22:45 +0100

    added preliminary test for mass conservation in kernel via Koletz-Olund

--- src/coag_solvers/v01_py/src/main.py
+++ src/coag_solvers/v01_py/src/main.py
@@ -1,3 +1,4 @@
+from numpy import test
 from tqdm import tqdm
 from termcolor import colored
 
@@ -24,6 +25,36 @@ def run_solver(cfg, K_gain, K_loss, x, n0):
     return ns
 
 
+def test_mass_conservation(cfg, K_gain, K_loss):
+    i = 0
+    j = 0
+    m_i = utils.mass_from_index(i, cfg.mass_grid_exp_min, cfg.mass_grid_stepsize)
+    m_j = utils.mass_from_index(j, cfg.mass_grid_exp_min, cfg.mass_grid_stepsize)
+    print("m_i =", m_i)
+    print("m_j =", m_j)
+
+    m = m_i + m_j
+    k_l = utils.index_from_mass(m, cfg.mass_grid_exp_min, cfg.mass_grid_stepsize)
+    k_h = k_l + 1
+    print("k_l =", k_l)
+    print("k_h =", k_h)
+
+    m_l = utils.mass_from_index(k_l, cfg.mass_grid_exp_min, cfg.mass_grid_stepsize)
+    m_h = utils.mass_from_index(k_h, cfg.mass_grid_exp_min, cfg.mass_grid_stepsize)
+
+    eps = (m_i + m_j - m_l) / (m_h - m_l)
+    print("eps =", eps)
+
+    a = m_i * K_gain[k_l][i][j] + m_j * K_gain[k_h][i][j]
+    b = (m_i + m_j) *  K_loss[i][i][j]
+    if a != b:
+        print()
+        print(a)
+        print(b)
+        if b == -2*a:
+            print("There is (at least) a factor 1/2 error here!")
+
+
 def main():
     print(colored("\nRunning solver v01_py...", "yellow"))
 
@@ -40,6 +71,7 @@ def main():
         cfg.mass_grid_stepsize,
         cfg.coagulation_kernel_variant
     )
+    test_mass_conservation(cfg, K_gain, K_loss)
 
     # Define initial state.
     x, n0 = state_initialization.initial_state(cfg)

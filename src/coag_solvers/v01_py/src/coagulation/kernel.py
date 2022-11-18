finally assured mass conservation! (I believe) -> relevant change: `calc_total_mass.py`, total mass calculation was wrong!

src/coag_solvers/v01_py/src/coagulation/kernel.py
@@ -10,8 +10,11 @@ def K(
     mass_grid_exp_min,
     mass_grid_stepsize,
     coagulation_kernel_variant,
+    run_stability_tests,
 ):
-    res = np.zeros(shape=(mass_grid_resolution, mass_grid_resolution, mass_grid_resolution))
+    K_gain = np.zeros(shape=(mass_grid_resolution, mass_grid_resolution, mass_grid_resolution))
+    K_loss = np.zeros(shape=(mass_grid_resolution, mass_grid_resolution, mass_grid_resolution))
+
     for i in range(mass_grid_resolution):
         m_i = mass_from_index(i, mass_grid_exp_min, mass_grid_stepsize)
         for j in range(mass_grid_resolution):
@@ -19,30 +22,71 @@ def K(
 
             # Determine masses before & after hit-and-stick collision.
             m_k = m_i + m_j
-            # Determine index of bins adjacent to combined mass.
+
+            # Determine indices of bins adjacent to combined mass.
             k_l = index_from_mass(m_k, mass_grid_exp_min, mass_grid_stepsize)
-            k_h = k_l + 1  
+            k_h = k_l + 1
+
+            # Check if indices of resulting mass(es) lie outside the discrete mass grid.
+            if k_l >= mass_grid_resolution or k_h >= mass_grid_resolution:
+                # print("i =", i, ", j =", j, "-> k_l =", k_l)
+                # print("                -> k_h =", k_h, "\n")
+                continue
+
             # Get mass corresponding to these indices.
             m_l = mass_from_index(k_l, mass_grid_exp_min, mass_grid_stepsize)
             m_h = mass_from_index(k_h, mass_grid_exp_min, mass_grid_stepsize)
 
             # Calculate loss term (gain term can then be calculated from this).
-            K_l = R(i, j, mass_grid_exp_min, mass_grid_stepsize, coagulation_kernel_variant)
+            R_kij = R(i, j, mass_grid_exp_min, mass_grid_stepsize, coagulation_kernel_variant)
 
             # Use linear ansatz to split kernel between adjacent next-lower/-higher bins.
             eps = (m_i + m_j - m_l) / (m_h - m_l)
 
-            if k_l == mass_grid_resolution or k_h == mass_grid_resolution:
-                continue  # TODO handle cases where `k_h == mass_grid_resolution`
-
             # Add gain-term to adjacent "next-lower" bin.
-            res[k_l][i][j] += 1/2 * K_l * (1-eps)
-            # Add gain-term to adjacent "next-higher" bin.
-            res[k_h][i][j] += 1/2 * K_l * eps
-            # Add loss term.
-            res[i][i][j] -= K_l
+            K_gain[k_l][i][j] += R_kij * (1-eps)
 
-    return res
+            # Add gain-term to adjacent "next-higher" bin.
+            K_gain[k_h][i][j] += R_kij * eps
+
+            # Add loss term.
+            K_loss[i][i][j] += R_kij
+
+    if run_stability_tests:
+        good, bad, skipped, total = 0, 0, 0, 0
+        for i in range(mass_grid_resolution):
+            m_i = mass_from_index(i, mass_grid_exp_min, mass_grid_stepsize)
+            for j in range(mass_grid_resolution):
+                m_j = mass_from_index(j, mass_grid_exp_min, mass_grid_stepsize)
+                m_k = m_i + m_j
+                k_l = index_from_mass(m_k, mass_grid_exp_min, mass_grid_stepsize)
+                k_h = k_l + 1
+                if k_l >= mass_grid_resolution or k_h >= mass_grid_resolution:
+                    # print("i =", i, ", j =", j, "-> k_l =", k_l)
+                    # print("                -> k_h =", k_h, "\n")
+                    skipped += 1
+                    continue
+                m_l = mass_from_index(k_l, mass_grid_exp_min, mass_grid_stepsize)
+                m_h = mass_from_index(k_h, mass_grid_exp_min, mass_grid_stepsize)
+
+                a = m_l * K_gain[k_l][i][j] + m_h * K_gain[k_h][i][j]
+                b = (m_i + m_j) * K_loss[i][i][j]
+                if a != b:
+                    if (a-b)/b > 10e-16:
+                        bad += 1
+                    else:
+                        good += 1
+                else:
+                    good += 1
+
+        total = good + bad + skipped
+        print("\tChecking mass conservation (eq. 1.21) up to 10^-16")
+        print("\tgood:    ", good, "\t/", total)
+        print("\tbad:     ", bad, "\t/", total)
+        print("\tskipped: ", skipped, "\t/", total)
+        print("\n\t-> Accuracy:", round(good/(bad+good)*100), "% (for non-skipped)")
+
+    return K_gain, K_loss
 
 
 @jit(nopython=True)

src/coag_solvers/v01_py/src/state_forwarding/__init__.py
@@ -1,22 +1,50 @@
+from pprint import pprint
+
 import numpy as np
 from numba import jit
 
+from utils.elementary_functions import kronecker_delta
+from utils.mass_index_conversion import mass_from_index
+
 
 @jit(nopython=True)
-def dn_k(K, n, k):
+def dn_k(K_gain, K_loss, n, k):
     res = 0
+
+    # Determine positive contribution to derivative from coagulation kernel's gain-term.
     for i in range(k+1):
-        for j in range(K.shape[0]):
-            res += K[k][i][j] * n[i] * n[j]
+        for j in range(k+1):
+            res += 1/2 * K_gain[k][i][j] * n[i] * n[j]
+
+    # Determine negative contribution to derivative from coagulation kernel's loss-term.
+    for i in range(K_loss.shape[0]):
+        for j in range(K_loss.shape[0]):
+            res -= K_loss[k][k][j] * n[i] * n[j] * kronecker_delta(k, i)
+
     return res
 
 
 @jit(nopython=True)
-def forward_state(K, x, n):
+def forward_state(
+    K_gain, 
+    K_loss, 
+    x, 
+    n, 
+    mass_grid_exp_min, 
+    mass_grid_stepsize,
+    run_stability_tests,
+):
     # Initialize mass-distribution derivative vector.
     dn = np.zeros(len(x))
 
     # Calulcate entries of derivative vector.
     for k, _ in enumerate(n):
-        dn[k] = dn_k(K, n, k)
+        dn[k] = dn_k(K_gain, K_loss, n, k)
+
+    if run_stability_tests:
+        dmdt = sum([
+            n_k * mass_from_index(k, mass_grid_exp_min, mass_grid_stepsize) 
+            for k, n_k in enumerate(dn)
+        ])
+        print("\n\t\tSum_k dn_k/dt = dm/dt =", dmdt)
     return n + dn

src/coag_solvers/v01_py/src/state_initialization/__init__.py
@@ -5,6 +5,7 @@ import utils
 
 def initial_state(cfg):
     x = np.logspace(cfg.mass_grid_exp_min, cfg.mass_grid_exp_max, cfg.mass_grid_resolution)
+    # print(x)
 
     if cfg.initial_mass_distribution == "gaussian":
         # mu, sigma = 10**(-5), 10**(-7)
@@ -12,7 +13,7 @@ def initial_state(cfg):
         raise Exception("TODO implement Gaussian in log-representation")
 
     elif cfg.initial_mass_distribution == "dirac-delta":
-        idx0 = 10
+        idx0 = 2
         n = utils.dirac_delta(x, idx0)
 
     else:
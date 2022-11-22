a165a4191e55a26b47b5e49ca53cf9e2fed534a4
Vincent C. Mader <vincent.mader@icloud.com>
2022-11-22 14:52:51 +0100

    minor

--- src/coag_solvers/v01_py/src/state_forwarding/__init__.py
+++ src/coag_solvers/v01_py/src/state_forwarding/__init__.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 import numpy as np
 from numba import jit
 
@@ -9,28 +7,32 @@ from utils.mass_index_conversion import mass_from_index
 
 @jit(nopython=True)
 def dn_k(K_gain, K_loss, n, k):
-    res = 0
+    dn_k = 0
 
     # Determine positive contribution to derivative from coagulation kernel's gain-term.
     for i in range(k+1):
         for j in range(k+1):
-            res += 1/2 * K_gain[k][i][j] * n[i] * n[j]
-
+            dn_k += 1/2 * K_gain[k][i][j] * n[i] * n[j]
     # Determine negative contribution to derivative from coagulation kernel's loss-term.
     for i in range(K_loss.shape[0]):
         for j in range(K_loss.shape[0]):
-            res -= K_loss[k][k][j] * n[i] * n[j] * kronecker_delta(k, i)
+            dn_k -= K_loss[k][k][j] * n[i] * n[j] * kronecker_delta(k, i)
 
-    return res
+    # for i in range(K_loss.shape[0]):
+    #     for j in range(K_loss.shape[0]):
+    #         K = 1/2 * K_gain[k][i][j] - K_loss[k][k][j] * kronecker_delta(k, i)
+    #         dn_k += K * n[i] * n[j]
+
+    return dn_k
 
 
 @jit(nopython=True)
 def forward_state(
-    K_gain, 
-    K_loss, 
-    x, 
-    n, 
-    mass_grid_exp_min, 
+    K_gain,
+    K_loss,
+    x,
+    n,
+    mass_grid_exp_min,
     mass_grid_stepsize,
     run_stability_tests,
 ):
@@ -43,7 +45,7 @@ def forward_state(
 
     if run_stability_tests:
         dmdt = sum([
-            n_k * mass_from_index(k, mass_grid_exp_min, mass_grid_stepsize) 
+            n_k * mass_from_index(k, mass_grid_exp_min, mass_grid_stepsize)
             for k, n_k in enumerate(dn)
         ])
         print("\n\t\tSum_k dn_k/dt = dm/dt =", dmdt)

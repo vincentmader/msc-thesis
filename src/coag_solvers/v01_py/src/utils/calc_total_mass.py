1bba16f0301ba77ed1bf482f8fe03cc33b209526
Vincent C. Mader <vincent.mader@icloud.com>
2022-11-22 10:13:05 +0100

    minor

--- src/coag_solvers/v01_py/src/utils/calc_total_mass.py
+++ src/coag_solvers/v01_py/src/utils/calc_total_mass.py
@@ -4,5 +4,10 @@ def calc_total_mass(x, n, mass_grid_exp_min, mass_grid_stepsize):
     m_tot = sum(
         mass_from_index(i, mass_grid_exp_min, mass_grid_stepsize) * n_i for i, n_i in enumerate(n)
     )
+
     # m_tot = sum(x_i * n_i for x_i, n_i in zip(x, n))
+
+    # for i, x_i in enumerate(x):
+    #     input(f"{x[i]} != {mass_from_index(i, mass_grid_exp_min, mass_grid_stepsize)}")
+
     return m_tot

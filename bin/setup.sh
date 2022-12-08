82a8c734e15e8f478ee3f66dd3e63299abd3c1c0
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-08 15:45:24 +0100

    added LaTeX header files

--- bin/setup.sh
+++ bin/setup.sh
@@ -16,9 +16,24 @@
 #   Create needed directories.
 #   ───────────────────────────────────────────────────────────────────────────
 
-#   Create `../out` directory, if it does not yet exist.
+#   Create `../out` directory, if it does not exist already.
     [ -d ../out ] || mkdir ../out
 
-#   Create `../out/runs` directory, if it does not yet exist.
+#   Create `../out/runs` directory, if it does not exist already.
     [ -d ../out/runs ] || mkdir ../out/runs
 
+
+#   Clone repository containing base header-files for LaTeX compilation.
+#   ───────────────────────────────────────────────────────────────────────────
+
+#   Define URL & file-path to tex-headers.
+    url="https://github.com/vincentmader/tex-headers"
+    path="../tex/tex-headers/"
+
+#   Clone repository, if it does not exist already.
+    if [ ! -d $path ]; then
+        git clone $url $path
+#   If it does, pull new changes.
+    else
+        cd $path && git pull
+    fi

11c6f68c006d3aec06a609e8f26fa398fc72355a
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-08 20:21:41 +0100

    Make sure `python3` is installed

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-#   Setup virtual environment for python.
+#   Setup python.
 #   ───────────────────────────────────────────────────────────────────────────
+
+#   Make sure required `python3` is installed. Abort if not.
+    if ! command -v python3 &> /dev/null; then
+        echo "ERROR: python3 could not be found"
+        exit
+    fi
 
 #   Create virtual environment for python, if it does not exist already.
     [ -d ../venv ] || python3 -m venv ../venv
 
-#   Enter virtual environment.
-    source ../venv/bin/activate;
-
 #   Install python-pip3 dependencies.
-    pip3 install -r ../requirements.txt
+    ../venv/bin/pip3 install -r ../requirements.txt
 
 
 #   Create needed directories.

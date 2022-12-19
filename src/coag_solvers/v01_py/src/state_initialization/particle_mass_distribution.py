f451d258e6ff0c3786806fd33757c7f647720519
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-20 00:10:04 +0100

    implemented "even flatter" initial conditions (-> $m\cdot N_i\cdot\Delta m=const. \forall i$)

--- src/coag_solvers/v01_py/src/state_initialization/particle_mass_distribution.py
+++ src/coag_solvers/v01_py/src/state_initialization/particle_mass_distribution.py
@@ -4,29 +4,35 @@ from config import Config
 import utils
 
 
-def get_initial_mass_distribution(cfg: Config, x):
+def get_initial_mass_distribution(cfg: Config, m):
     r"""Constructs initial particle mass distribution $N(m)=n(m)\cdot\Delta m$.
 
     Arguments:
     - cfg:  simulation configuration
-    - x:    mass grid
+    - m:    mass grid
     """
     variant = cfg.initial_mass_distribution
+    resolution = cfg.mass_grid_resolution
+    assert resolution == m.shape[0]
 
     if variant == "dirac-delta":
         idx0 = 2  # TODO
-        return utils.dirac_delta(x, idx0)
+        return utils.dirac_delta(m, idx0)
 
     elif variant == "gaussian":
         mu = 1e-5  # TODO
         sigma = 1e-7  # TODO
-        return utils.gaussian(x, mu, sigma)
+        return utils.gaussian(m, mu, sigma)
 
     elif variant == "flat":
-        return np.ones(x.shape[0]) / x.shape[0]
+        return np.ones(resolution) / cfg.mass_grid_resolution
 
     elif variant == "flatter":
-        return np.ones(x.shape[0]) / x.shape[0]
+        if cfg.mass_grid_variant == "linear":
+            raise Exception()
+        elif cfg.mass_grid_variant == "logarithmic":
+            C = 1 / (np.sum(1 / m))
+            return C / m
 
     else:
         raise Exception(f"Unknown mass distribution \"{variant}\".")

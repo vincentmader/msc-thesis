be2fd9a0431acd678383712753fcc03ba8b347ea
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-19 22:46:48 +0100

    removed test for mass conservation

--- src/coag_kernels/v01_py/src/kernel_construction/coagulation/__init__.py
+++ src/coag_kernels/v01_py/src/kernel_construction/coagulation/__init__.py
@@ -12,38 +12,3 @@ def kernel(cfg):
         K = K_gain + K_loss
 
     return K
-
-
-# def test_for_mass_conservation(cfg, K):
-#     good, bad, skipped, total = 0, 0, 0, 0
-#     for i in range(K.shape[0]):
-#         m_i = mass_from_index(i, cfg)
-#         for j in range(K.shape[1]):
-#             m_j = mass_from_index(j, cfg)
-#             m_k = m_i + m_j
-#             k_l = index_from_mass(m_k, cfg)
-#             k_h = k_l + 1
-#             if max(k_l, k_h) >= cfg.mass_grid_resolution:
-#                 # print("i =", i, ", j =", j, "-> k_l =", k_l)
-#                 # print("                -> k_h =", k_h, "\n")
-#                 skipped += 1
-#                 continue
-#             m_l = mass_from_index(k_l, cfg)
-#             m_h = mass_from_index(k_h, cfg)
-
-#             a = m_l * K[k_l][i][j] + m_h * K[k_h][i][j]
-#             b = (m_i + m_j) * K[i][i][j]
-#             if a != b:
-#                 if (a-b)/b > 10e-16:
-#                     bad += 1
-#                 else:
-#                     good += 1
-#             else:
-#                 good += 1
-
-#     total = good + bad + skipped
-#     print("\tChecking mass conservation (eq. 1.21) up to 10^-16")
-#     print("\tgood:    ", good, "\t/", total)
-#     print("\tbad:     ", bad, "\t/", total)
-#     print("\tskipped: ", skipped, "\t/", total)
-#     print("\n\t-> Accuracy:", round(good/(bad+good)*100), "% (for non-skipped)")
